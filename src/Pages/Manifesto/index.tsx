3d3b22f3d0afb28ed21daf3a694f8bf6f30ec8d2
Douglas <douglasbgavioli@hotmail.com>
2022-10-01 17:49:35 -0300

    formulario

--- src/Pages/Manifesto/index.tsx
+++ src/Pages/Manifesto/index.tsx
@@ -1,77 +1,90 @@
+import "./style.min.css"
+
 import { Input } from "../../Components/Input";
 
 export function Manifesto() {
+
     return (
         <div className="cr-manifesto">
             <h1>MANIFESTO DE CARGA E SERVIÇO!</h1>
             <form className="cr-manifesto__form" onSubmit={() => console.log("enviado O FORMULARIO DO FELIPE COCOZAO")}>
                 <div className="cr-manifesto__form-basicos">
-                    <label>
-                        DATA
-                        <Input placeholder="teste" />
-                    </label>
-                    <label>
-                        NOME DO CLIENTE
-                        <Input placeholder="teste" />
-                    </label>
-                    <label>
-                        ENDEREÇO
-                        <Input placeholder="teste" />
-                    </label>
-                    <label>
-                        NOME DO EQUIPAMENTO
-                        <Input placeholder="teste" />
-                    </label>
-                    <label>
-                        OPERADOR
-                        <Input placeholder="teste" />
-                    </label>
-                    <label>
-                        TELEFONE
-                        <Input placeholder="teste" />
-                    </label>
+                    <div>
+                        <label>
+                            Data
+                            <Input type={"date"} />
+                        </label>
+                        <label>
+                            Nome do cliente
+                            <Input placeholder="Digite um nome" />
+                        </label>
+                        <label>
+                            Endereço
+                            <Input placeholder="Digite um endereço" />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Nome do equipamento
+                            <Input placeholder="Digite o nome do equipamento" />
+                        </label>
+                        <label>
+                            Operador
+                            <Input placeholder="Digite o nome do operador" />
+                        </label>
+                        <label>
+                            Telefone
+                            <Input type={"number"} placeholder="55-99999-9999" />
+                        </label>
+                    </div>
                 </div>
                 <div className="cr-manifesto__form-horimetro">
                     <label>
                         <p>HORIMETRO</p>
-                        INICIO
-                        <Input placeholder="teste" />
-                        FIM
-                        <Input placeholder="teste" />
+                        <div className="horas">
+                            Início
+                            <Input placeholder="Hora inicial" />
+                            Fim
+                            <Input placeholder="Hora final" />
+                        </div>
                     </label>
-                    <h1>HORAS TRABALHADAS(OBS)</h1>
-                    (calculo inicio - fim)
-                    <Input type={"checkbox"} />PAGO
+                    <label>
+                        <p className="horas-trabalhadas">HORAS TRABALHADAS</p>
+                        {/* (calculo inicio - fim) */}
+                        <span>21.3 Horas</span>
+                        <div className="cr-manifesto__form-horimetro-pago">
+                            Serviço pago?
+                            <Input type={"checkbox"} className="checkbox" />
+                        </div>
+                    </label>
+
                     <label >
                         <p> VALOR TOTAL DO SERVIÇO</p>
-                        PREÇO DA HORA
-                        <Input placeholder="teste" />
-                        (calculo baseado no valor da hora)
+                        Preço da hora
+                        <Input placeholder="Digite o preço da hora" />
+                        {/* (calculo baseado no valor da hora) */}
+                        7.000.00
                     </label>
                     <label >
                         OBSERVAÇÕES
-                        <textarea style={{ resize: "none" }} cols={30} rows={10} ></textarea>
+                        <textarea style={{ resize: "none" }} cols={30} rows={5} ></textarea>
                     </label>
                 </div>
-                <div className="cr-manifesto__form-nCargas">
-                    <textarea style={{ resize: "none" }}></textarea>
+                <div className="cr-manifesto__form-carro">
+                    <div className="cr-manifesto__form-carro-abastecimento">
+                        <h1>Abastecimento</h1>
+                        <div>
+                            <p>Litros de diesel abastecidos no dia</p>
+                            <Input type={"number"} placeholder="Quantidade de combustivel" />
+                        </div>
+                        <button type="submit">Enviar</button>
+                    </div>
+                    <div className="cr-manifesto__form-carro-manutencoes">
+                        <p>Anotação das manutenções</p>
+                        <textarea cols={50} rows={10}></textarea>
+                    </div>
                 </div>
-                <div className="cr-manifesto__form-origem">
-                    <label>
-                        ORIGEM
-                        <Input type={"text"} />
-                        DESTINO
-                        <Input type={"text"} />
-                        MATERIAL
-                        <Input type={"checkbox"} />ORGÂNICO
-                        <Input type={"checkbox"} />ARGILA/TERRA
-                        <Input type={"checkbox"} />BOTA FORA
-                        <Input type={"checkbox"} />AREIA
-                        <Input type={"checkbox"} />ROCHA
-                        <Input type={"checkbox"} />CASCALHO
-                    </label>
-                </div>
-                <button type="submit">Clica envias</button>
+
             </form>
         </div>
     )
